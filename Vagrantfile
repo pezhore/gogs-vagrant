e940fe01f2edcf6f6cb99f3b5b743b09607d76f3
pezhore <pezhore@gmail.com>
2018-02-16 08:56:12 -0600

    Trying to update for Workstation/v0.11.34

--- Vagrantfile
+++ Vagrantfile
@@ -2,12 +2,11 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu-16.04-amd64"
+  config.vm.box = "bento/ubuntu-16.04"
 
   config.vm.network "private_network", ip: "192.168.33.10"
 
-  config.vm.provider "virtualbox" do |vb|
-    vb.linked_clone = true
+  config.vm.provider "vmware_workstation" do |vb|
     #vb.gui = true
     vb.memory = "1024"
   end
--- build-gogs.sh
+++ build-gogs.sh
@@ -4,7 +4,7 @@ export GOPATH=$PWD/gopath
 export PATH=$GOPATH/bin:$PATH
 if [ ! -d $GOPATH/src/github.com/gogits/gogs ]; then
   mkdir -p $GOPATH/{src,pkg,bin}
-  git clone -b v0.11.29 https://github.com/gogits/gogs $GOPATH/src/github.com/gogits/gogs
+  git clone -b v0.11.34 https://github.com/gogits/gogs $GOPATH/src/github.com/gogits/gogs
   sed -i -E 's/(\s+)(cmd.Admin,)/\1\2\n\1cmd.CreateAdminUser,/' $GOPATH/src/github.com/gogits/gogs/gogs.go
   ln -s /vagrant/create-admin-user.go $GOPATH/src/github.com/gogits/gogs/cmd/
 fi
